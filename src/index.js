03f479978598a57ce06e0c38f55bd25056de86a1
Harsh Mohite <harsh.mohite009@gmail.com>
2023-07-09 21:55:10 +0530

    refactor: removed unnecessary context

--- src/index.js
+++ src/index.js
@@ -5,18 +5,15 @@ import "./index.css";
 import App from "./App";
 
 import { BrowserRouter as Router } from "react-router-dom";
-import AppContextProvider from "./context/AppContextProvider";
 import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Router>
-    <AppContextProvider>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </AppContextProvider>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </Router>
   // </React.StrictMode>
 );
